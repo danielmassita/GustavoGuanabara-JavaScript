42c4bc59b99cfb9209269cd0d8d5297253888bf6
danielmassita <daniel.massita@gmail.com>
2023-01-19 14:43:40 -0300

    Aula 01 Iniciando...

--- Aula_01.js
+++ Aula_01.js
@@ -1,2 +1,7 @@
-// Aula 01 - 
-// Oi! 
+// Aula 1 – O que o JavaScript é capaz de fazer?
+// https://www.youtube.com/watch?v=Ptbk2af68e8
+
+/*
+Você sabe para que serve a linguagem JavaScript / ECMAScript? Sabe a diferença entre um cliente e um servidor para a Internet? Sabe para que servem as tecnologias HTML5, CSS3 e JavaScript? Consegue citar 4 sites que utilizam a linguagem JavaScript nos seus códigos?
+Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
+*/
