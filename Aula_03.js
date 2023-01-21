01e70a70607e5757f37b2d776f486993721de3dd
danielmassita <daniel.massita@gmail.com>
2023-01-20 22:52:34 -0300

    Aula 03 Iniciando

---
+++ Aula_03.js
@@ -0,0 +1,9 @@
+// Aula 03 - Dando os Primeiros Passos
+// https://youtu.be/FdePtO5JSd0
+/* 
+Quais são os melhores livros de JavaScript em Português? Onde ter acesso à documentação oficial do JavaScript em Português e Inglês? Quais são os requisitos de software para aprender a programar em JavaScript? Qual é o melhor editor para códigos JavaScript?  Como instalar o Node.js no seu computador? Como configurar o Node.js? Para aprender JavaScript, é realmente necessário saber muito Inglês?Você está precisando de dicas para estudar e aprender de verdade?
+
+Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
+
+Aula do Curso de JavaScript e ECMAScript para Iniciantes, criado pelo professor Gustavo Guanabara para o canal CursoemVideo.
+*/
