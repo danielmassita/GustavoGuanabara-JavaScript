c5261d5cf9847d28b49e2f0abd438a87f60489ef
danielmassita <daniel.massita@gmail.com>
2023-01-21 23:27:33 -0300

    Aula 04 perguntas última aula...

--- Aula_04.js
+++ Aula_04.js
@@ -1,2 +1,19 @@
 // Aula 04 - Criando o seu primeiro script
 // https://youtu.be/OmmJBfcMJA8
+
+/*
+Você já sabe diferenciar dentro do seu código, os trechos em HTML5, em CSS3 e em JavaScript? Sabe organizar as pastas do seu projeto dentro do Visual Studio Code? Sabe como testar se o Node.js está devidamente instalado? Já sabe utilizar os comandos alert, confirm e prompt do JavaScript? 
+
+Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
+
+Aula do Curso de JavaScript e ECMAScript para Iniciantes, criado pelo professor Gustavo Guanabara para o canal CursoemVideo.
+*/
+
+/* 
+PERGUNTAS AULA ANTERIOR
+- Qual a bibliografia recomendada? (2 livros (FLANAGAN e SILVA) e 2 sites guias de referência (ECMA e Mozilla)...
+- Precisa saber inglês? Não, é balela...
+- Quais dicas pra estudar? Ver aos vídeos + escrever e codar + ler manuais, refs., artigos + fazer anotações + estudar juntes + criar projetos próprios + PERSISTIR!
+- Quais programas pra instalar? Google Chrome (navegador) + VS Code (escritor de códigos) + Node.js (backend). 
+*/
+
