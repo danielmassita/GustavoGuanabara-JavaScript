3ba9f3a691cd9ff2b73adf6d5c07087263b66828
danielmassita <daniel.massita@gmail.com>
2023-01-23 22:43:36 -0300

    Aula 06 Perguntas aula anterior...

--- Aula_06.js
+++ Aula_06.js
@@ -1,3 +1,10 @@
 // Aula 06 - Tratamento de dados - Curso JavaScript #06
 // https://www.youtube.com/watch?v=OJgu_KCCUSY
 
+/*
+PERGUNTAS AULA ANTERIOR
+- O que é uma variável? É um espaço na memória, que se atribui um dado, e pode ser alterado, somado, computado, etc...
+- Como declarar uma variável numérica inteira ou real usando JS? var numeroInt = 10; var numeroReal = 5.5...
+- Quais são os tipos primitivos do JS? String, Number, Boolean.
+- O que significa colocar o valor null dentro de uma variável? Atribuir um espaço "vacio" pra variável, que curiosamente receberá o typeof de 'object'...
+*/
