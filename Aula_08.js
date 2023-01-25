093ce829442032a9d5d94047c596b75096a45fbb
danielmassita <daniel.massita@gmail.com>
2023-01-24 23:42:24 -0300

    Aula 08 Iniciando...

---
+++ Aula_08.js
@@ -0,0 +1,9 @@
+// Aula 08 - Operadores (Parte 2) - Curso JavaScript #08
+// https://youtu.be/BP63NhITvao
+
+/* Você já conhece os operadores relacionais e os operadores lógicos em JavaScript? Sabe a diferença entre usar = ou == ou === em JavaScript? Conhece a ordem de precedência dos operadores do JavaScript? Sabe como usar o operador ternário para atribuições em JavaScript?
+
+Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
+
+Aula do Curso de JavaScript e ECMAScript para Iniciantes, criado pelo professor Gustavo Guanabara para o canal CursoemVideo. */
+
