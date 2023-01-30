1eaf13ad7ec145f4d05d08c0209b73064e2308ea
danielmassita <daniel.massita@gmail.com>
2023-01-30 16:25:58 -0300

    Aula 09 Iniciando...

---
+++ Aula_09.js
@@ -0,0 +1,9 @@
+// Aula 09 - Introdução ao DOM - Curso JavaScript #09
+// https://youtu.be/WWZX8RWLxIk
+
+/* Você sabe o que significa a sigla DOM? Sabe para que serve o Document Object Model? Sabe como criar uma árvore DOM para o seu site? Sabe como usar o JavaScript para manipular o DOM? Sabe para que servem os elementos Parent e Child em um DOM? Sabe utilizar os métodos getElementByTagName, getElementById, getElementByName, getElementByClassName, querySelector de acesso ao DOM no JavaScript?
+
+Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
+
+Aula do Curso de JavaScript e ECMAScript para Iniciantes, criado pelo professor Gustavo Guanabara para o canal CursoemVideo.*/
+
