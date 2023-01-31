295a99b699a3ba7eb18c8f11cbc180bbf64283e4
danielmassita <daniel.massita@gmail.com>
2023-01-31 15:42:25 -0300

    Aula 10 Iniciando...

---
+++ Aula_10.js
@@ -0,0 +1,12 @@
+// Aula 10 - Eventos DOM - Curso JavaScript #10
+// https://youtu.be/wWnBB-mZIvY
+
+/* Você já entendeu como funciona o DOM com JavaScript? Sabe criar funções em JavaScript? Consegue ligar uma função a um evento em um formulário HTML5 usando JavaScript? Sabe como pegar valores dentro de caixas de texto e fazer cálculos com eles? 
+
+Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
+
+Aula do Curso de JavaScript e ECMAScript para Iniciantes, criado pelo professor Gustavo Guanabara para o canal CursoemVideo.*/
+
+/* PERGUNTAS AULA ANTERIOR
+- 
+*/
