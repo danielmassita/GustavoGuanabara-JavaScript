a85f35c810f6b5c2a43926879d9ed452ab0244a5
danielmassita <daniel.massita@gmail.com>
2023-02-02 22:06:18 -0300

    Aula 11 Iniciando...

---
+++ Aula_11.js
@@ -0,0 +1,12 @@
+// Aula 11 - Condições (Parte 1) - Curso JavaScript #11
+// https://youtu.be/cOdG4eACN2A
+
+/* Você sabe usar o comando if no JavaScript? Sabe diferenciar condições simples e condições compostas? Sabe como criar uma condição usando JavaScript? Sabe como instalar e configurar a extensão Node Exec no Visual Studio Code?
+
+Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
+
+Aula do Curso de JavaScript e ECMAScript para Iniciantes, criado pelo professor Gustavo Guanabara para o canal CursoemVideo. */
+
+/* PERGUNTAS DA AULA ANTERIOR
+- 
+*/
