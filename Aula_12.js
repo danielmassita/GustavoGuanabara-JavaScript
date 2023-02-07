ac0a5671a998727eac2d7a64163434a6f82b06cb
danielmassita <daniel.massita@gmail.com>
2023-02-07 00:32:13 -0300

    Aula 12 Estudando...

--- Aula_12.js
+++ Aula_12.js
@@ -8,13 +8,32 @@ Pois, para responder a essas e muitas outras perguntas, assista essa aula do Cur
 Aula do Curso de JavaScript e ECMAScript para Iniciantes, criado pelo professor Gustavo Guanabara para o canal CursoemVideo. */
 
 /* PERGUNTAS AULA ANTERIOR
-- Você sabe utilizar a extensão "Node Exec" no VSCode? 
 
+- Você sabe utilizar a extensão "Node Exec" no VSCode? Temos que selecionar o código a ser testado, e apertar o F8/F9 (iniciar/finalizar) para rodar no terminal apenas o trecho selecionado.
 
-- Você sabe criar uma condição em JavaScript (e operadores relacionais, ==, ===)? 
+- Você sabe criar uma condição em JavaScript (e operadores relacionais, ==, ===)? Sim, a condição em JS é descrita assim (abaixo). Códigos com igualdade restrita === vão testar a condição inclusive para o valor do dado E TAMBÉM o tipo (typeof). Porém, com igualdade == apenas o valor do dado será testado. Por exemplo:
 
+    if (condição) {
+        códigos para true...
+    } else {
+        códigos para false...
+    }
 
 - Consegue diferenciar condições SIMPLES e condições COMPOSTAS? 
+
+if (condição1 == true) {
+    código de ação Z aqui
+} // caso contrário, nada de especial acontece e o código segue o fluxo, sem ter executado a ação Z se for FALSA a condição1...
+
+if (condição 2 == true) {
+    código de ação A aqui para TRUE...
+} else {
+    código de ação B aqui para FALSE...
+} // a condição composta vai dar uma alternativa de execução B pra quando a premissa for falsa... */
+
+
+/* `${`Condições em JS} - CONDIÇÕES Parte 2`
+
 
 
 */
