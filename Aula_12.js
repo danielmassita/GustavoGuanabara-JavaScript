18bb5bd787eb90786a74a94fb0f55c3ceac9790a
danielmassita <daniel.massita@gmail.com>
2023-02-06 23:55:45 -0300

    Aula 12 Iniciando...

---
+++ Aula_12.js
@@ -0,0 +1,20 @@
+// Aula 12 - Condições (Parte 2) - Curso JavaScript #12
+// https://youtu.be/EEStcIe8rAM
+
+/* Você sabe mudar a cor de fundo de um site, dinamicamente, usando JavaScript? Sabe como inserir uma imagem usando JavaScript em um site, sem ter a tag img previamente definida? Sabe como utilizar condições simples (if), condições compostas (if..else), condições aninhadas (if..elseif..else) e condições múltiplas (switch..case) em JavaScript?
+
+Pois, para responder a essas e muitas outras perguntas, assista essa aula do Curso de JavaScript para Iniciantes até o final. E não se esqueça sempre de praticar todas as atividades que fizermos durante o vídeo no seu próprio computador.
+
+Aula do Curso de JavaScript e ECMAScript para Iniciantes, criado pelo professor Gustavo Guanabara para o canal CursoemVideo. */
+
+/* PERGUNTAS AULA ANTERIOR
+- Você sabe utilizar a extensão "Node Exec" no VSCode? 
+
+
+- Você sabe criar uma condição em JavaScript (e operadores relacionais, ==, ===)? 
+
+
+- Consegue diferenciar condições SIMPLES e condições COMPOSTAS? 
+
+
+*/
