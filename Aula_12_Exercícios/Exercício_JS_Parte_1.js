17f11269b00ff6b3dd7c206f0245e98466fa8a0b
danielmassita <daniel.massita@gmail.com>
2023-02-07 23:27:41 -0300

    Exercício JS Parte 1

---
+++ Aula_12_Exercícios/Exercício_JS_Parte_1.js
@@ -0,0 +1,22 @@
+// Exercícios JavaScript (Parte 1) - Curso JavaScript
+// https://youtu.be/b2K7eo5Jdj8
+
+/*
+
+MODELO DE SITE
+
+body
+
+    header
+        Título
+
+    section
+        Área do Exercício
+        div
+        div
+
+    footer
+        Rodapé
+
+*/
+
