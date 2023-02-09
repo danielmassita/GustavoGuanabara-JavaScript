7ddfab891a13e78335a312afe2f90edc4845f3b6
danielmassita <daniel.massita@gmail.com>
2023-02-09 17:03:57 -0300

    Exercício 014 FIM

--- Aula_12_Exercícios/ex014/script.js
+++ Aula_12_Exercícios/ex014/script.js
@@ -3,17 +3,20 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 4 // hard coded test
+//    var hora = 11 // hard coded test
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora > 5 && hora <= 12) {
         // Bom dia!
         img.src = 'fotomanha.png'
+        document.body.style.background = 'rgb(250, 195, 90)'
     } else if (hora > 12 && hora < 19) {
         // Boa tarde!
         img.src = 'fototarde.png'
+        document.body.style.background = 'rgb(180, 75, 55)'
     } else {
         // Boa noite!
         img.src = 'fotonoite.png'
+        document.body.style.background = 'rgb(4, 59, 123'
     }
 }
     
