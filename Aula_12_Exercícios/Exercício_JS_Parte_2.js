7a5b290b15b223d3dbfa8517debfc8402f5993aa
danielmassita <daniel.massita@gmail.com>
2023-02-09 16:17:21 -0300

    Exercício 014 Andamento...

---
+++ Aula_12_Exercícios/Exercício_JS_Parte_2.js
@@ -0,0 +1,8 @@
+// Exercícios JavaScript (Parte 2) - Curso JavaScript
+// https://youtu.be/UXSWgnbSHxs
+
+/* `${Hora de Praticar}`
+
+- Abre os arquivos da aula passada e Mãos à Obra, Mãos na Massa!
+- Dentro da Aula 12 Ex 
+*/
