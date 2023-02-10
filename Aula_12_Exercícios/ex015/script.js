9874607c75a7b3b0c713992d623435d44469f186
danielmassita <daniel.massita@gmail.com>
2023-02-10 17:02:22 -0300

    Aula 012 Ex 015

--- Aula_12_Exercícios/ex015/script.js
+++ Aula_12_Exercícios/ex015/script.js
@@ -1,1 +1,56 @@
-alert("Olá!")
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.querySelector('div#res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano)
+    window.alert('[ERRO] Verifique os dados e tente novamente!')
+    else {
+        // window.alert('Tudo ok.') validação básica pro ano menor que o atual e maior que zero.
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        // res.innerHTML = `Idade calculada: ${idade} anos.` Teste resultado de seleção name='radsex' no plural pra getElementsByName alterando a div 'res'.
+
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos um(a) ${gênero} com ${idade} anos.`
+        res.appendChild(img) // Colocamos esse comando pra ADICIONAR via JS um elemento adicional na div 'res'.
+    }
+
+}
