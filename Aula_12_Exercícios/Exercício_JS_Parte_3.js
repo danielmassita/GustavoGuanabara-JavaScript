86a4e1d4412cbbaf24e43099807f8b3886cd193d
danielmassita <daniel.massita@gmail.com>
2023-02-10 16:17:39 -0300

    Aula 012 Ex 015

---
+++ Aula_12_Exercícios/Exercício_JS_Parte_3.js
@@ -0,0 +1,20 @@
+// Exercícios JavaScript (Parte 3) - Curso JavaScript
+// https://youtu.be/f5es-PpaUI8
+
+/* `${Hora de Praticar}`
+
+- Abre os arquivos da aula passada e Mãos à Obra, Mãos na Massa!
+- Dentro da Aula 12 Ex 015
+- Buscar e tratar imagens - fundo transparente em *.png, círculo de avatar (border-radius), tamanho 250x250 px, de:
+    bebe masculino
+    bebe feminino
+    criança masculino
+    criança feminino
+    adulto masculino
+    adulto feminino
+    idoso masculino
+    idoso feminino
+
+*/
+
+/* TRANSCRIÇÃO */
