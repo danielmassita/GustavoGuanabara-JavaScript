8a6c2054c2e51e2f86c0722d3f6d151eb72fc9a6
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-02-18 23:43:48 -0300

    Update Aula_14.js

--- Aula_14.js
+++ Aula_14.js
@@ -1,2 +1,10 @@
 // Aula 14 - Repetições (Parte 2) - Curso JavaScript #14
 // https://youtu.be/eX-lkN_Zahc
+
+/* PERGUNTAS AULA ANTERIOR
+1) O que eu faço se quiser fazer uma determinada atividade acontecer várias vezes? 
+2) Sabe diferenciar uma estrutura de condição  e as estruturas de repetição? 
+3) Consegue dizer a diferença entre as estruturas WHILE e DO..WHILE? 
+
+
+*/
