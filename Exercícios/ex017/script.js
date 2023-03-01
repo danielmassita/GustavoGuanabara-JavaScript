f3f0fad52e38b67d4a622843bc716d17aea7095f
Bianca Pacheco Nunes <112512785+BiaPacNun@users.noreply.github.com>
2023-03-01 16:22:36 -0300

    Pasta Modelo

---
+++ Exercícios/ex017/script.js
@@ -0,0 +1,20 @@
+// window.alert(`Olá, Mundo!`)
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` // faz muito sentido o item com value pro PHP
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
