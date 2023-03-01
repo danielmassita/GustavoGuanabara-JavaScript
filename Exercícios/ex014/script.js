ac10eeb2f5f2ef746474c1cd67ac8b1e88b4963b
Bianca Pacheco Nunes <112512785+BiaPacNun@users.noreply.github.com>
2023-03-01 01:35:00 -0300

    Pasta Modelo

--- Exercícios/ex014/script.js
+++ Exercícios/ex014/script.js
@@ -4,13 +4,13 @@ function carregar() {
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('imagem');
     var data = new Date();
-    //var hora = 10 // teste brute force...
     var hora = data.getHours();
+    //var hora = 22 // teste brute force...
     msg.innerHTML = `Agora são exatamente ${hora} hora(s).`   
     if (hora >= 6 && hora <= 12) {
         // Bom dia!
         img.src = 'fotomanha.png'
-        document.body.style.background = '#fee88e'
+        document.body.style.background = '#fedf78'
     } else if (hora > 12 && hora < 18) {
         // Boa tarde!
         img.src = 'fototarde.png'
