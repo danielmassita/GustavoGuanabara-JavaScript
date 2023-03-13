aae4b772379dca5fceb57a235bd01a55fc045839
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-03-13 00:30:49 -0300

    Create

---
+++ Exercícios/Exercício_JS_Parte_7.js
@@ -0,0 +1,7 @@
+// Exercícios JavaScript (Parte 7) - Curso JavaScript
+// https://youtu.be/vEOEZ03ZyiE
+
+/* PERGUNTAS AULA ANTERIOR
+1) Já aprendeu a usar a extensão do VSCode Watch In Chrome? 
+2) Já aprendeu a usar o Modo de Depuração do VSCode?
+3) Já treinou bastante com os componentes Array e Function? */
